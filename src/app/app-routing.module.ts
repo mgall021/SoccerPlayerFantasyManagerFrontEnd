2b5bd4d9cce24c39fab52bb140ccfc42b79e6fc6
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-26 03:44:59 -0700

    create routing for Create Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
   {
-    path:"",
-    component: LandingComponent
-  }
- 
-
+    path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'create',
+    component: CreateComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
