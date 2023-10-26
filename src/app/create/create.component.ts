950b5fd827554a5c20de35a4d74317680261b39d
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-26 13:45:30 -0700

    create service for making a fantasy team

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,51 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'; // Import the Router for navigation
+import { Router } from '@angular/router';
 import { FantasyTeamService } from '../fantasy-team.service';
+import { SoccerPlayerService } from '../soccer-player.service';
 import { SoccerPlayer } from '../soccer-player.model';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
   userFantasyTeamPlayers: SoccerPlayer[] = [];
   playerSearchTerm: string = '';
   searchResults: SoccerPlayer[] = [];
-  hasFantasyTeam: boolean = false; 
+  hasFantasyTeam: boolean = false;
 
   constructor(
     private router: Router,
     private fantasyTeamService: FantasyTeamService
-  ) { }
+  ) {}
 
   ngOnInit() {
-=
     this.checkUserHasFantasyTeam();
   }
 
-=
   checkUserHasFantasyTeam() {
-
-    this.fantasyTeamService.getFantasyTeams(userId).subscribe((fantasyTeams) => {
-      this.hasFantasyTeam = fantasyTeams.length > 0;
-      if (!this.hasFantasyTeam) {
-
-        this.router.navigate(['/create-fantasy-team']);
-      }
-    });
+    this.fantasyTeamService
+      .getUserFantasyTeams(userId)
+      .subscribe((fantasyTeams) => {
+        this.hasFantasyTeam = fantasyTeams.length > 0;
+        if (!this.hasFantasyTeam) {
+          this.router.navigate(['/create-fantasy-team']);
+        }
+      });
   }
   loadUserFantasyTeamPlayers() {
-    this.fantasyTeamService.getFantasyTeams(userId).subscribe((fantasyTeams) => {
-      if (fantasyTeams.length > 0) {
-        this.userFantasyTeamPlayers = fantasyTeams[0].soccerPlayers; // list of players
-      }
-    });
+    this.fantasyTeamService
+      .getUserFantasyTeams(userId)
+      .subscribe((fantasyTeams) => {
+        if (fantasyTeams.length > 0) {
+          this.userFantasyTeamPlayers = fantasyTeams[0].soccerPlayers; // list of players
+        }
+      });
   }
   searchPlayers() {
-    this.fantasyTeamService.searchPlayers(this.playerSearchTerm).subscribe((results) => {
-      this.searchResults = results;
-    });
+    this.fantasyTeamService
+      .searchPlayers(this.playerSearchTerm)
+      .subscribe((results) => {
+        this.searchResults = results;
+      });
   }
   addPlayer(playerId: number) {
     this.fantasyTeamService.addPlayerToTeam(userId, playerId).subscribe(() => {
@@ -54,10 +57,10 @@ export class CreateComponent implements OnInit {
     });
   }
   removePlayer(playerId: number) {
-    this.fantasyTeamService.removePlayerFromTeam(userId, playerId).subscribe(() => {
-      this.loadUserFantasyTeamPlayers();
-    });
+    this.fantasyTeamService
+      .removePlayerFromTeam(userId, playerId)
+      .subscribe(() => {
+        this.loadUserFantasyTeamPlayers();
+      });
   }
-
-
 }
--- src/app/fantasy-team.service.ts
+++ src/app/fantasy-team.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FantasyTeamService {
+  private baseUrl = 'http://localhost:9098/api/fantasyTeam';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUserFantasyTeams(userId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${userId}`);
+  }
+
+  createFantasyTeam(fantasyTeam: any): Observable<any> {
+    return this.http.post(this.baseUrl, fantasyTeam);
+  }
+
+  addPlayerToTeam(teamId: number, playerId: number): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${teamId}/addPlayer/${playerId}`, {});
+  }
+
+  removePlayerFromTeam(teamId: number, playerId: number): Observable<any> {
+    return this.http.put(
+      `${this.baseUrl}/${teamId}/removePlayer/${playerId}`,
+      {}
+    );
+  }
+
+  deleteFantasyTeam(teamId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${teamId}`);
+  }
 }
