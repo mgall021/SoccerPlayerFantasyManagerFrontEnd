af16fbf0d952cb3307de6330d0e55755b630f04f
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-25 23:23:08 -0700

    modify description files for correct imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FantasyTeamDescriptionComponent } from './landing/fantasy-team-description/fantasy-team-description.component';
-
-
 
 @NgModule({
-  declarations: [AppComponent, FantasyTeamDescriptionComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, RouterModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
