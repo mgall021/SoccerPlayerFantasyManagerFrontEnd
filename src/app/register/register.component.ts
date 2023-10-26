8c6456f1e725c5ed89c8905274ee7e488387c272
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-26 10:14:15 -0700

    implement registration logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,26 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
   registerForm: any = {};
+  constructor(private httpClient: HttpClient) {}
 
 
   onRegisterSubmit() {
-  
-  }
+    name: this.registerForm.name, 
+    emailAddress: this.registerForm.emailAddress, 
+    password: this.registerForm.password,
+  };
+
+  this.httpClient.post('/auth/users/register/', registrationData)
+  .subscribe(
+    (response: any) => {
+      // Registration successful, handle the response
+      console.log(response); // You can log the response for debugging
+      // Handle the success response (e.g., show a success message to the user)
+    },
+    (error: any) => {
+      // Registration failed, handle the error
+      console.error(error); // Log the error for debugging
+      // Handle the error (e.g., display an error message to the user)
+    }
+  );
 }
