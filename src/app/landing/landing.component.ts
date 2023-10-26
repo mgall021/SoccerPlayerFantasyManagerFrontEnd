6b9ce83ff81be3ba7ccdd20795d38701f7fa251e
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-25 21:13:58 -0700

    import carousel components inside of landing compoent

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
