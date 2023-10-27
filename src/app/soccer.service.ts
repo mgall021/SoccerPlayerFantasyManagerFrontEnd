8fca0dc222cd4b9944dec71abeb19d9568a52b23
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-26 23:29:27 -0700

    confure soccer service to include 2 controllers

--- src/app/soccer.service.ts
+++ src/app/soccer.service.ts
@@ -1,9 +1,40 @@
+// soccer.service.ts
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SoccerService {
+  private baseUrl = 'http://localhost:9098';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  searchPlayersByCriteria(criteria: string, query: string) {
+    return this.http.get(
+      `${this.baseUrl}/api/soccerplayers/${criteria}/${query}`
+    );
+  }
+
+  addPlayerToTeam(teamId: number, playerId: number) {
+    return this.http.put(
+      `${this.baseUrl}/api/fantasyTeam/${teamId}/addPlayer/${playerId}`,
+      {}
+    );
+  }
+
+  removePlayerFromTeam(teamId: number, playerId: number) {
+    return this.http.put(
+      `${this.baseUrl}/api/fantasyTeam/${teamId}/removePlayer/${playerId}`,
+      {}
+    );
+  }
+
+  getUserTeams(userId: number) {
+    return this.http.get(`${this.baseUrl}/api/fantasyTeam/${userId}`);
+  }
+
+  createFantasyTeam(team: any) {
+    return this.http.post(`${this.baseUrl}/api/fantasyTeam`, team);
+  }
 }
