8955910bf21ae5f9f39115e34207285689e3ba6d
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-27 22:43:35 -0700

    create doc strings for login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,25 +8,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  /**
+   * Object containing user login details.
+   * @property {string} emailAddress - The email address of the user.
+   * @property {string} password - The password of the user.
+   */
   loginData = {
     emailAddress: '',
     password: '',
   };
 
-  constructor(private authService: AuthService, private router: Router) {}
+  /**
+   * Constructs the component and injects required services.
+   * @param {AuthService} authService - Service for authentication related operations.
+   * @param {Router} router - Angular Router for navigation.
+   */
+  constructor(private authService: AuthService, private router: Router) {} // inject AuthService
 
+  /**
+   * Handles the submission of the login form.
+   * Attempts to authenticate the user with provided credentials and navigates to the '/create' route on success.
+   * Displays error information if the login attempt fails.
+   */
   onLoginSubmit() {
-    this.authService.login(this.loginData.emailAddress, this.loginData.password).subscribe(
-      (response) => {
-        console.log('Login successful:', response);
-        console.log('JWT Token:', localStorage.getItem('token'));
-        console.log('Before navigating to /create...');
-        this.router.navigate(['/create']);
-        console.log('After navigating to /create...');
-      },
-      (error) => {
-        console.error('Login failed:', error);
-      }
-    );
+    this.authService
+      .login(this.loginData.emailAddress, this.loginData.password)
+      .subscribe(
+        (response) => {
+          console.log('Login successful:', response);
+          console.log('JWT Token:', localStorage.getItem('token'));
+          console.log('Before navigating to /create...');
+          this.router.navigate(['/create']);
+          console.log('After navigating to /create...');
+        },
+        (error) => {
+          console.error('Login failed:', error);
+        }
+      );
   }
 }
