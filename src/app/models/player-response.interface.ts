5c2f8f6a6d90c5da6106813482ec1dcb204bbad1
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-27 22:49:26 -0700

    code cleanuo

--- src/app/models/player-response.interface.ts
+++ src/app/models/player-response.interface.ts
@@ -1,22 +1,27 @@
-// Assuming the file is named player-response.interface.ts or similar
+/**
+ * Represents the details of a soccer player.
+ */
 
 export interface Player {
-    id: number;
-    name: string;
-    position: string;
-    team: string;
-    country: string;
-    rating: number;
-  }
-  
-  export interface PlayerResponse {
-    data: Player[];
-    message: string;
-  }
-  
-  export interface TeamResponse {
-    id: number;
-    name: string;
-    soccerPlayers: Player[];
-  }
-  
+  id: number;
+  name: string;
+  position: string;
+  team: string;
+  country: string;
+  rating: number;
+}
+/**
+ * Represents the response structure for player related API calls.
+ */
+export interface PlayerResponse {
+  data: Player[];
+  message: string;
+}
+/**
+ * Represents the details and structure of a soccer fantasy team.
+ */
+export interface TeamResponse {
+  id: number;
+  name: string;
+  soccerPlayers: Player[]; // used for grabbibg the correct info each player
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,27 +1,48 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../auth.service'; 
+import { AuthService } from '../auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  /**
+   * Object containing user registration details.
+   * @property {string} name - The full name of the user.
+   * @property {string} emailAddress - The email address of the user.
+   * @property {string} password - The password chosen by the user.
+   */
   registerData = {
     name: '',
     emailAddress: '',
-    password: ''
+    password: '',
   };
 
-  constructor(private authService: AuthService,private route:ActivatedRoute, private router: Router) {}
+  /**
+   * Constructs the component and injects required services.
+   * @param {AuthService} authService - Service for authentication related operations.
+   * @param {ActivatedRoute} route - Represents the active route associated with the component that is loaded in an outlet.
+   * @param {Router} router - Angular Router for navigation.
+   */
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {} // injections
 
+  /**
+   * Handles the submission of the registration form.
+   * Attempts to register the user with provided details and navigates to the '/login' route on successful registration.
+   * Displays error information if the registration attempt fails.
+   */
   onRegisterSubmit() {
     this.authService.register(this.registerData).subscribe(
       (response) => {
         console.log(response.data.id);
         this.authService.setUserId(response.data.id);
-        this.router.navigate(['/login']);
+        this.router.navigate(['/login']); // matches with backend output
       },
       (error) => {
         console.error('Registration failed:', error);
